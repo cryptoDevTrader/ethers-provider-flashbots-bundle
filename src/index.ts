e755c89306952fa69195acf41e488b3662a2c636
Christopher M. Mitchell <christopher@divthis.com>
2023-03-10 16:44:27 -0800

    Making bundleHash optional.

--- src/index.ts
+++ src/index.ts
@@ -365,7 +365,7 @@ export class FlashbotsBundleProvider extends providers.JsonRpcProvider {
           opts?.minTimestamp
         ),
       receipts: () => this.fetchReceipts(bundleTransactions),
-      bundleHash: response.result.bundleHash
+      bundleHash: response.result.bundleHash || null
     }
   }
 
